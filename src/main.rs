95b8860a62f362f7beff5be8bf5e773d2be7b0e0
Alexandria <28266167+alxpettit@users.noreply.github.com>
2023-06-19 16:26:29 -0600

    eh good enough

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
-use nix::unistd::Pid;
 use snafu::prelude::*;
 use snafu::Whatever;
 use std::env;
 use std::ffi::OsString;
 use std::fs;
 use std::os::unix::prelude::CommandExt;
-use std::process::{exit, Command};
+use std::process::Command;
 
+#[snafu::report]
 fn main() -> Result<(), Whatever> {
-    match env::args().collect::<Vec<String>>().as_slice() {
-        [_, pid_str, child_program, child_args @ ..] => {
+    let args: Vec<String> = env::args().collect();
+    let args_str: Vec<&str> = args.iter().map(AsRef::as_ref).collect();
+    match &args_str[..] {
+        [_self, "--help"] => {
+            eprintln!("Usage: runlike <pid> <command> [<arg1> <arg2> ...]");
+        }
+        [_self, pid_str, child_program, child_args @ ..] => {
             let pid = pid_str
                 .parse::<i32>()
                 .with_whatever_context(|_| format!("Invalid PID: {}", pid_str))?;
 
             let env_path = format!("/proc/{}/environ", pid);
             let env_vars = fs::read_to_string(&env_path)
-                .with_whatever_context(|_| format!("Failed to read from file"))?;
+                .with_whatever_context(|_| format!("Failed to read from file: {}", env_path))?;
 
             for env_var in env_vars.split('\0') {
                 if let Some(equal_index) = env_var.find('=') {
@@ -30,7 +35,7 @@ fn main() -> Result<(), Whatever> {
         }
         _ => {
             eprintln!("Usage: runlike <pid> <command> [<arg1> <arg2> ...]");
-            exit(1);
+            whatever!("Wrong number of args. Provided: {}", args_str.len() - 1);
         }
     }
     Ok(())
