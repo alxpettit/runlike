17bc146e99fef021571787b3e1f09efdedea2eb8
Alexandria <28266167+alxpettit@users.noreply.github.com>
2023-06-19 18:45:11 -0600

    good enough

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,55 @@
-use libc::setuid;
+use clap::Parser;
 use snafu::prelude::*;
 use snafu::Whatever;
 use std::env;
-use std::ffi::OsString;
-use std::fs;
 use std::os::unix::prelude::CommandExt;
 use std::process::Command;
+use sysinfo::{Pid, ProcessExt, System, SystemExt};
 
-fn how_to_use() {
-    eprintln!("Usage: runlike <uid> <pid> <command> [<arg1> <arg2> ...]");
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    pid: Pid,
+    #[clap(required = true)]
+    child_exe: String,
+    #[clap(required = false)]
+    child_args: Vec<String>,
 }
+
 #[snafu::report]
 fn main() -> Result<(), Whatever> {
-    let args_string: Vec<String> = env::args().collect();
-    let args_str: Vec<&str> = args_string.iter().map(AsRef::as_ref).collect();
-    match &args_str[..] {
-        [_self, "--help"] | [_self, "-h"] => {
-            how_to_use();
-        }
-        [_self, uid_str, pid_str, child_program, child_args @ ..] => {
-            let uid = uid_str
-                .parse::<u32>()
-                .with_whatever_context(|_| format!("Invalid UID: {}", uid_str))?;
+    let args = Args::parse();
+    let sys = System::new_all();
 
-            let pid = pid_str
-                .parse::<i32>()
-                .with_whatever_context(|_| format!("Invalid PID: {}", pid_str))?;
+    let process = sys
+        .process(args.pid)
+        .with_whatever_context(|| format!("Could not find process with ID: {}", args.pid))?;
 
-            let env_path = format!("/proc/{}/environ", pid);
-            let env_vars = fs::read_to_string(&env_path)
-                .with_whatever_context(|_| format!("Failed to read from file: {}", env_path))?;
+    let uid = process
+        .user_id()
+        .with_whatever_context(|| format!("Unable to retrieve UID of process: {:?}", process))?;
 
-            for env_var in env_vars.split('\0') {
-                if let Some(equal_index) = env_var.find('=') {
-                    let (key, value) = env_var.split_at(equal_index);
-                    env::set_var(key, &value[1..]);
-                }
-            }
+    let gid = process
+        .group_id()
+        .with_whatever_context(|| format!("Unable to retrieve GID of process: {:?}", process))?;
 
-            let child_args: Vec<OsString> = child_args.iter().map(|arg| arg.into()).collect();
-
-            unsafe {
-                if setuid(uid) != 0 {
-                    whatever!("Failed to call setuid.");
-                }
-            }
-            Command::new(child_program).args(&child_args).exec();
-        }
-        _ => {
-            how_to_use();
-            whatever!("Wrong number of args. Provided: {}", args_str.len() - 1);
+    for env_var in process.environ() {
+        if let Some(equal_index) = env_var.find('=') {
+            let (key, value) = env_var.split_at(equal_index);
+            env::set_var(key, &value[1..]);
         }
     }
+
+    unsafe {
+        if libc::setuid(**uid) != 0 {
+            whatever!("Failed to setuid for UID: {:?}", uid);
+        }
+        if libc::setgid(*gid) == 0 {
+            whatever!("Failed to setgid for GID: {:?}", gid);
+        }
+    }
+
+    Command::new(args.child_exe).args(args.child_args).exec();
+
     Ok(())
 }
