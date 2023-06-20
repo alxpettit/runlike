1af506514af28bcb924e10ea745ae770c986ba8f
Alexandria <28266167+alxpettit@users.noreply.github.com>
2023-06-19 20:26:53 -0600

    making progress

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,33 @@
 
 use clap::Parser;
 use snafu::prelude::*;
+use snafu::ResultExt;
 use snafu::Whatever;
 use std::env;
 use std::os::unix::prelude::CommandExt;
 use std::process::Command;
-use sysinfo::{Pid, ProcessExt, System, SystemExt, UserExt};
+use sysinfo::{Pid, Process, ProcessExt, System, SystemExt, User, UserExt};
 
 #[derive(Parser, Debug)]
 struct Args {
     #[arg(short, long)]
-    pid: Pid,
+    pid: Option<Pid>,
     #[clap(required = true)]
     child_exe: String,
     #[clap(required = false)]
     child_args: Vec<String>,
 }
 
-#[snafu::report]
-fn main() -> Result<(), Whatever> {
-    let args = Args::parse();
-    let sys = System::new_all();
+// startplasma-wayland
+// startplasma-x11
 
-    let process = sys
-        .process(args.pid)
-        .with_whatever_context(|| format!("Could not find process with ID: {}", args.pid))?;
+fn detect_process<'a>(sys: &'a System) -> Result<&'a Process, Whatever> {
+    // for (pid, process) in sys.processes() {}
 
+    Err(whatever!("sdssd"))
+}
+
+fn get_user_from_proc<'a>(sys: &'a System, process: &Process) -> Result<&'a User, Whatever> {
     let uid = process
         .effective_user_id()
         .with_whatever_context(|| format!("Unable to retrieve UID of process: {:?}", process))?;
@@ -34,16 +36,34 @@ fn main() -> Result<(), Whatever> {
     let user = sys
         .get_user_by_id(uid)
         .with_whatever_context(|| format!("Could not resolve user name from ID: {:?}", uid))?;
+    Ok(user)
+}
+
+#[snafu::report]
+fn main() -> Result<(), Whatever> {
+    let args = Args::parse();
+    let sys = System::new_all();
+
+    let process = match args.pid {
+        Some(pid) => sys
+            .process(pid)
+            .with_whatever_context(|| format!("Could not find process with ID: {:?}", args.pid))?,
+        None => detect_process(&sys)
+            .with_whatever_context(|_| format!("Can't detect process to use."))?,
+    };
+
+    let user = get_user_from_proc(&sys, &process)?;
+    let name = user.name();
+    let groups = user.groups();
+    let environ = process.environ();
 
     privdrop::PrivDrop::default()
-        .user(user.name())
-        .group_list(user.groups())
+        .user(name)
+        .group_list(groups)
         .apply()
-        .with_whatever_context(|_| {
-            format!("Could not set privs to match target user: {}", user.name())
-        })?;
+        .with_whatever_context(|_| format!("Could not set privs to match target user: {}", name))?;
 
-    for env_var in process.environ() {
+    for env_var in environ {
         if let Some(equal_index) = env_var.find('=') {
             let (key, value) = env_var.split_at(equal_index);
             env::set_var(key, &value[1..]);
